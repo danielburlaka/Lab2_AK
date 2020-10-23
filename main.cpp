4121628f8a3098aa580ec0084b665956fd76fdff
danielburlaka <61365322+danielburlaka@users.noreply.github.com>
2020-10-23 22:27:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "calculator.h"
+
+using namespace std;
+
+int main(int argc, char** argv[]) {
+	double a;
+	double a;
+	Calculator calculator;
+	cout << "Enter first number: " << endl;
+	cin >> a;
+	cout << "Enter second number: " << endl;
+	cin >> b;
+	cout << "Result:  " << calculator.Sub(a, b) << endl;
+
+	return 0;	
+}
